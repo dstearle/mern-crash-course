1d1b8e4a0908c8316499acdda1491abe49ff619f
dstearle <dallas.st.earle@gmail.com>
2020-03-30 08:36:16 -0700

    Created new file for the navbar

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+
+    state = {
+
+        isOpen = false
+
+    }
+
+    toggle = () => {
+
+        this.state({
+
+            isOpen: !this.state.isOpen
+
+        });
+
+    }
+
+    render(){
+
+        <div>
+
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+
+                <Container>
+
+                    <NavbarBrand href="/">ShoppingList</NavbarBrand>
+
+                    <NavbarToggler onClick={this.toggle} />
+
+                    <Collapse isOpen={this.state.isOpen} navbar>
+
+                        <Nav className="ml-auto" navbar>
+
+                            <NavItem>
+
+                                <NavLink href="https://github.com/dstearle/mern-crash-course">Github</NavLink>
+
+                            </NavItem>
+
+                        </Nav>
+
+                    </Collapse>
+
+                </Container>
+
+            </Navbar>
+
+        </div>
+
+    }
+
+}
+
+export default AppNavbar;
