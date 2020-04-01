cfc41df98387105ff730a0272c2800f80fff41fe
dstearle <dallas.st.earle@gmail.com>
2020-04-01 00:50:04 -0700

    Created new file for holding the state for items

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,14 @@
+import {v4 as uuidv4} from 'uuid';
+
+const initialState = {
+
+    items: [
+
+        {id: uuidv4(), name: 'Eggs'},
+        {id: uuidv4(), name: 'Milk'},
+        {id: uuidv4(), name: 'Steak'},
+        {id: uuidv4(), name: 'Water'},
+
+    ]
+
+}
